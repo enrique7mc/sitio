bd6cdb31be9552175a16209bf4b1721797373ff4
enrique7mc <enrique7mc@gmail.com>
2015-09-11 21:08:58 -0500

    Añade index.php
    Esta página princial

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>My primer sitio PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hola Mundo!</p>";
+        ?>
+        <a href="login.php"> Click here to login </a> <br />
+        <a href="register.php"> Click here to register </a> 
+    </body>
+</html>
